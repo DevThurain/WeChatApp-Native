f8591653df87d982c89010c1d103f15f244a7999
thurain <thurainbowin.dev@gmail.com>
2022-12-21 13:54:19 +0630

    Moment View Holder UI

---
+++ app/src/main/java/com/thurainx/wechat_app/views/view_holders/MomentViewHolder.kt
@@ -0,0 +1,11 @@
+package com.thurainx.wechat_app.views.view_holders
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class MomentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bind(){
+
+    }
+}
